d5db4979f412626c7cb1532f0a7a3ddc7c6c278c
Benny Ho <benny.ho2530@gmail.com>
2019-06-24 16:56:55 +0200

    added code for cards

--- javascript.js
+++ javascript.js
@@ -3,6 +3,14 @@ const cards = document.querySelectorAll(".memory-card");
 cards.forEach(card => card.addEventListener("click", flipCard));
 
 function flipCard() {
-  console.log("I was clicked!");
-  console.log(this);
+  this.classList.toggle('flip');
+
+  if (!hasFlippedCard) {
+    // first click
+
+    hasFlippedCard = true;
+    firstCard = this;
+
+    console.log(hasFlippedCard, firstCard);
+  }
 }
