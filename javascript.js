33441426cfa701749acdb8b93e3f09cd274a15b8
Benny Ho <benny.ho2530@gmail.com>
2019-06-24 12:17:30 +0200

    added cards and images

---
+++ javascript.js
@@ -0,0 +1,9 @@
+var banana = document.querySelector(".banaan");
+var apple = document.querySelector(".apple");
+var strawberry = document.querySelector(".strawberry");
+var kiwi = document.querySelector(".kiwi");
+var card = document.querySelector(".card");
+
+card.addEventListener("click", card);
+
+function cardBack() {}
